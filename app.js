35a4bc23d4bacb34394115c27db4d4a35225ef73
Beata Tomczak <tma.btty@gmail.com>
2020-10-12 14:31:46 +0200

    Generate random candy type

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 8
     const squares = []
 
+    const candyTypes = [
+        '🍭',
+        '🍍',
+        '🍓',
+        '🧁',
+        '🍬',
+        '🍇'
+    ]
+
     // Create Board
     function createBoard() {
         for (let i = 0; i < width*width; i++) {
             const square = document.createElement('div')
+            let randomType = Math.floor(Math.random() * candyTypes.length)
+      
+            square.innerText = candyTypes[randomType]
+            square.classList.add('square')
             grid.appendChild(square)
             squares.push(square)
         }
