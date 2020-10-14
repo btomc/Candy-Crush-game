b5e525079d23b8bc6cb75af70c0d35d1b9a63ec5
Beata Tomczak <tma.btty@gmail.com>
2020-10-14 23:32:26 +0200

    Check for matches - Three & Four

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const width = 8
     const squares = []
+    let score = 0
 
     const candyTypes = [
         '🍭',
@@ -90,6 +91,93 @@ document.addEventListener('DOMContentLoaded', () => {
             squares[squareIdBeingDragged].innerText = typeBeingDragged
         } else squares[squareIdBeingDragged].innerText = typeBeingDragged
     }
+
+    // Checking for matches
+    // check for row of Four
+    function checkRowForFour() {
+        for (i = 0; i < 61; i++) {
+            let rowOfFour = [i, i+1, i+2, i+3]
+            let decidedType = squares[i].innerText
+            const isBlank = squares[i].innerText === ''
+
+            const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55]
+            if (notValid.includes(i)) continue
+
+            if (rowOfFour.every(index => squares[index].innerText === decidedType && !isBlank)) {
+                score += 4
+                rowOfFour.forEach(index => {
+                    squares[index].innerText = ''
+                })
+            }                 
+        }
+    }
+
+    checkRowForFour()
+
+    // check for column of Four
+    function checkColumnForFour() {
+        for (i = 0; i < 39; i++) {
+            let columnOfFour = [i, i+width, i+width*2, i+width*3]
+            let decidedType = squares[i].innerText
+            const isBlank = squares[i].innerText === ''
+
+            if (columnOfFour.every(index => squares[index].innerText === decidedType && !isBlank)) {
+                score += 4
+                columnOfFour.forEach(index => {
+                    squares[index].innerText = ''
+                })
+            }                 
+        }
+    }
+
+    checkColumnForFour()
+
+
+    // check for row of Three
+    function checkRowForThree() {
+        for (i = 0; i < 62; i++) {
+            let rowOfThree = [i, i+1, i+2]
+            let decidedType = squares[i].innerText
+            const isBlank = squares[i].innerText === ''
+
+            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55]
+            if (notValid.includes(i)) continue
+
+            if (rowOfThree.every(index => squares[index].innerText === decidedType && !isBlank)) {
+                score += 3
+                rowOfThree.forEach(index => {
+                    squares[index].innerText = ''
+                })
+            }                 
+        }
+    }
+
+    checkRowForThree()
+
+    // check for column of Three
+    function checkColumnForThree() {
+        for (i = 0; i < 47; i++) {
+            let columnOfThree = [i, i+width, i+width*2]
+            let decidedType = squares[i].innerText
+            const isBlank = squares[i].innerText === ''
+
+            if (columnOfThree.every(index => squares[index].innerText === decidedType && !isBlank)) {
+                score += 3
+                columnOfThree.forEach(index => {
+                    squares[index].innerText = ''
+                })
+            }                 
+        }
+    }
+
+    checkColumnForThree()
+
+    window.setInterval(function(){
+        checkRowForFour()
+        checkColumnForFour()
+        checkRowForThree()
+        checkColumnForThree()
+    }, 100)
 
     
 
